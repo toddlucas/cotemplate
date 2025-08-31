26f5cffdf72723be2adc2f48fb3ab7619c7c6fe1
Todd Lucas <tl@toddlucas.net>
2025-08-31 16:11:54 -0700

    Server template fixups

--- src/server/Corp.Data/src/reset-npgsql-corp-database.cmd
+++ src/server/Corp.Data/src/reset-npgsql-corp-database.cmd
@@ -1,4 +1,4 @@
-psql -U postgres -a -c "DROP DATABASE IF EXISTS templateplaceholder.1"
+psql -U postgres -a -c "DROP DATABASE IF EXISTS corp"
 rd /q /s ..\..\Corp.Data.Npgsql\src\Migrations\Corp
-call add-npgsql-templateplaceholder.1-migration Initial
-call update-npgsql-templateplaceholder.1-database
+call add-npgsql-corp-migration Initial
+call update-npgsql-corp-database
--- src/server/Corp.Data/src/reset-sqlite-corp-database.cmd
+++ src/server/Corp.Data/src/reset-sqlite-corp-database.cmd
@@ -1,5 +1,5 @@
 
-del ..\..\Corp.Web\src\templateplaceholder.1.db
+del ..\..\Corp.Web\src\corp.db
 rd /q /s ..\..\Corp.Data.Sqlite\src\Migrations\Corp
-call add-sqlite-templateplaceholder.1-migration Initial
-call update-sqlite-templateplaceholder.1-database
+call add-sqlite-corp-migration Initial
+call update-sqlite-corp-database
