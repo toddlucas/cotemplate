919f456fbd471a33830dabe3f7721db56e45c340
Todd Lucas <tl@toddlucas.net>
2025-08-31 16:32:35 -0700

    Update client packages and port

--- src/client/app/vite.config.ts
+++ src/client/app/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  cacheDir: '.vite',
+  resolve: {
+    alias: {
+      // "@": path.resolve(__dirname, "./src"),
+      "$": path.resolve(__dirname, "../lib/src"),
+    },
+  },
+  server: {
+    port: 8383,
+  },
 })
