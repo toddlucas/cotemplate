4b3c9980bbc42c80959d000793440c0508ac0918
Todd Lucas <tl@toddlucas.net>
2025-08-31 23:22:35 -0700

    Add shared components for auth feature

---
+++ src/client/lib/src/components/index.ts
@@ -0,0 +1,7 @@
+export { Link } from './Link';
+export {
+  default as ErrorBoundary,
+  useErrorHandler,
+  withErrorBoundary
+} from './ErrorBoundary';
+export { default as ErrorBoundaryExample } from './ErrorBoundaryExample';
