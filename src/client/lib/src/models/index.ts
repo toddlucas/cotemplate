de540b11dffebf4fa6b0ba4fedc5976924a57178
Todd Lucas <tl@toddlucas.net>
2025-08-31 23:24:10 -0700

    Add base generation spec for barrels

---
+++ src/client/lib/src/models/index.ts
@@ -0,0 +1,9 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export * from './identity-user-model';
+export * from './problem-details-model';
+export * from './validation-problem-details-model';
+export * from './auth';
---
+++ src/server/Corp.Web/src/Models/BaseGenerationSpec.cs
@@ -0,0 +1,48 @@
+﻿using TypeGen.Core.SpecGeneration;
+
+namespace Corp.Models;
+
+public class BaseGenerationSpec : GenerationSpec
+{
+    public override void OnBeforeBarrelGeneration(OnBeforeBarrelGenerationArgs args)
+    {
+        AddBarrel(".", BarrelScope.Files); // adds one barrel file in the global TypeScript output directory containing only files from that directory
+        AddBarrel(".", BarrelScope.Files | BarrelScope.Directories); // equivalent to AddBarrel("."); adds one barrel file in the global TypeScript output directory containing all files and directories from that directory
+
+        // the following code, for each directory, creates a barrel file containing all files and directories from that directory
+        IEnumerable<string> directories = GetAllDirectoriesRecursive(args.GeneratorOptions.BaseOutputDirectory)
+            .Select(x => GetPathDiff(args.GeneratorOptions.BaseOutputDirectory, x));
+
+        foreach (string directory in directories)
+        {
+            AddBarrel(directory);
+        }
+
+        AddBarrel(".");
+    }
+
+    private string GetPathDiff(string pathFrom, string pathTo)
+    {
+        var pathFromUri = new Uri("file:///" + pathFrom?.Replace('\\', '/'));
+        var pathToUri = new Uri("file:///" + pathTo?.Replace('\\', '/'));
+
+        return pathFromUri.MakeRelativeUri(pathToUri).ToString();
+    }
+
+    private IEnumerable<string> GetAllDirectoriesRecursive(string directory)
+    {
+        var result = new List<string>();
+        string[] subdirectories = Directory.GetDirectories(directory);
+
+        if (!subdirectories.Any()) return result;
+
+        result.AddRange(subdirectories);
+
+        foreach (string subdirectory in subdirectories)
+        {
+            result.AddRange(GetAllDirectoriesRecursive(subdirectory));
+        }
+
+        return result;
+    }
+}
