438410cc9309fb7ce381590e98cd18fbf7f8b46d
Todd Lucas <tl@toddlucas.net>
2025-09-07 22:32:24 -0700

    Export new enumerations

--- src/server/Corp.Web/src/Models/AppGenerationSpec.cs
+++ src/server/Corp.Web/src/Models/AppGenerationSpec.cs
@@ -1,11 +1,14 @@
-﻿using TypeGen.Core.SpecGeneration;
-
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.BearerToken;
 using Microsoft.AspNetCore.Identity.Data;
 
 using Corp.Http;
 using Corp.Pagination;
+using Corp.Access;
+using Corp.Business;
+using Corp.Operations;
+using Corp.Storage;
+using Corp.Workflow;
 
 namespace Corp.Models;
 
@@ -37,5 +40,33 @@ public class AppGenerationSpec : BaseGenerationSpec
 
         AddInterface<PagedQuery>();
         AddInterface(typeof(PagedResult<>));
+
+        // Access enums
+        AddEnum<OrganizationMemberRole>();
+
+        // Business enums
+        AddEnum<EntityType>();
+        AddEnum<EntityRelationshipType>();
+        AddEnum<EntityRoleType>();
+        AddEnum<EntityStatus>();
+        AddEnum<OwnershipModel>();
+
+        // Operations enums
+        AddEnum<AiActionType>();
+        AddEnum<AuditAction>();
+        AddEnum<AiChannel>();
+        AddEnum<FeedbackTargetType>();
+
+        // Storage enums
+        AddEnum<DocumentCategory>();
+
+        // Workflow enums
+        AddEnum<ReminderStatus>();
+        AddEnum<SourceType>();
+        AddEnum<ReminderChannel>();
+        AddEnum<ChecklistStatus>();
+        AddEnum<ChecklistScope>();
+        AddEnum<Priority>();
+        AddEnum<Workflow.TaskStatus>();
     }
 }
